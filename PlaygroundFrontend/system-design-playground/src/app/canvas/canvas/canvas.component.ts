6c14fe8e1d29579d3f713c9e9a2b6544cb318992
mayank <123.ultron.1234@gmail.com>
2026-06-11 15:33:36 +0530

    fix: fixes node and edge alignments.

--- PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
+++ PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CdkDrag } from '@angular/cdk/drag-drop';
-import { NgFor } from '@angular/common';
+import { CdkDrag, CdkDragEnd } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-canvas',
-  imports: [CdkDrag, NgFor],
+  imports: [CdkDrag],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css'
 })
@@ -13,16 +12,25 @@ export class CanvasComponent {
   nodes = [
     { id: 1, type: 'Client', x: 24, y: 90 },
     { id: 2, type: 'Load Balancer', x: 24 + 200, y: 90 },
-    { id: 3, type: "API Server", x: 24 + 400, y: 90 }
+    { id: 3, type: "API Server", x: 24 + 400, y: 90 },
+    { id: 4, type: "DB", x: 24 + 600, y: 90 }
   ];
   edges = [
     { source: 1, target: 2 },
-    { source: 2, target: 3 }
+    { source: 2, target: 3 },
+    { source: 3, target: 4 }
   ]
 
   // helper getNode
   getNode(id: number) {
     return this.nodes.find(node => node.id == id);
+  }
+  // on drag stop
+  onDragEnd(event: CdkDragEnd, node: any) {
+    const pos = event.source.getFreeDragPosition();
+    node.x = pos?.x ?? 0;
+    node.y = pos?.y ?? 0;
+    console.log(pos);
   }
 
 }
