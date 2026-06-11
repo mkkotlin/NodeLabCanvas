5d6503378c5ac200254928cec1789d417ddbd6d4
mayank <123.ultron.1234@gmail.com>
2026-06-11 15:14:59 +0530

    feat: implement CanvasComponent with initial node and edge configuration for drag-and-drop system design layout

--- PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
+++ PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
@@ -1,29 +1,28 @@
 import { Component } from '@angular/core';
 import { CdkDrag } from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-canvas',
-  imports: [CdkDrag],
+  imports: [CdkDrag, NgFor],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css'
 })
 export class CanvasComponent {
-  pos_y = 90;
-  pos_x = 24;
-
   // Default nodes
   nodes = [
-    { id: 1, type: 'Client', x: this.pos_x, y: this.pos_y },
-    { id: 2, type: 'Load Balancer', x: this.pos_x + 200, y: this.pos_y },
-    { id: 3, type: "API Server", x: this.pos_x + 400, y: this.pos_y }
+    { id: 1, type: 'Client', x: 24, y: 90 },
+    { id: 2, type: 'Load Balancer', x: 24 + 200, y: 90 },
+    { id: 3, type: "API Server", x: 24 + 400, y: 90 }
   ];
   edges = [
     { source: 1, target: 2 },
     { source: 2, target: 3 }
-  ];
+  ]
 
   // helper getNode
   getNode(id: number) {
-    return this.nodes.find(node => node.id === id);
+    return this.nodes.find(node => node.id == id);
   }
+
 }
