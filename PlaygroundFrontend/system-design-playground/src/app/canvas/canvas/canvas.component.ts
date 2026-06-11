fc19a0a0b41337da74fd428923149fa57a87cbfb
mayank <123.ultron.1234@gmail.com>
2026-06-11 14:32:16 +0530

    feat: initialize system design playground with angular and draggable canvas nodes

--- PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
+++ PlaygroundFrontend/system-design-playground/src/app/canvas/canvas/canvas.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-canvas',
-  imports: [],
+  imports: [CdkDrag, NgFor],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css'
 })
 export class CanvasComponent {
+  pos_y = 90;
+  pos_x = 24;
+
+  // Default nodes
+  nodes = [
+    { id: 1, type: 'Client', x: this.pos_x, y: this.pos_y },
+    { id: 2, type: 'Load Balancer', x: this.pos_x + 200, y: this.pos_y },
+    { id: 3, type: "API Server", x: this.pos_x + 400, y: this.pos_y }
+  ];
 
 }
